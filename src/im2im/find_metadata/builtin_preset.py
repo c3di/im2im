25cbcf8c0ccde736fd59ded6f7d5b29d0182fb3a
Max-ChenFei <boxchenfei@gmail.com>
2024-08-02 15:51:19 +0200

    Add builtin preset

--- src/im2im/find_metadata/builtin_preset.py
+++ src/im2im/find_metadata/builtin_preset.py
@@ -12,10 +12,25 @@ skimage_lib = Metadata4Library(
     {"data_representation": "numpy.ndarray", "color_channel": "rgb", "channel_order": "channel last",
      "minibatch_input": False, "device": "cpu"})
 skimage_lib.add_preset_with_override_metadata("gray", {"color_channel": "gray", "channel_order": "none"})
-preset_table.add_lib_metadata("ski", skimage_lib)
+preset_table.add_lib_metadata("skimage", skimage_lib)
 
 pil_lib = Metadata4Library(
     {"data_representation": "PIL.Image", "color_channel": "rgb", "channel_order": "channel last",
      "minibatch_input": False, "device": "cpu"})
 pil_lib.add_preset_with_override_metadata("gray", {"color_channel": "gray", "channel_order": "none"})
 preset_table.add_lib_metadata("pil", pil_lib)
+
+numpy_lib = Metadata4Library(
+    {"data_representation": "numpy.ndarray", "color_channel": "rgb", "channel_order": "channel last",
+     "minibatch_input": False, "device": "cpu"})
+numpy_lib.add_preset_with_override_metadata("gray", {"color_channel": "gray", "channel_order": "none"})
+numpy_lib.add_preset_with_override_metadata("rgb_uint8", {"image_data_type": "uint8"})
+preset_table.add_lib_metadata("numpy", numpy_lib)
+
+torch_lib = Metadata4Library(
+    {"data_representation": "torch.tensor", "color_channel": "rgb", "channel_order": "channel first",
+     "minibatch_input": True, "image_data_type": "float32(0to1)", "device": "cpu"})
+torch_lib.add_preset_with_override_metadata("rgb_gpu", {"color_channel": "rgb", "device": "gpu"})
+torch_lib.add_preset_with_override_metadata("gray", {"color_channel": "gray"})
+torch_lib.add_preset_with_override_metadata("gray_gpu", {"color_channel": "gray", "device": "gpu"})
+preset_table.add_lib_metadata("torch", torch_lib)
